9807cad795abcaf80334babdffbafa85e8ec6c15
fr0ste <73338az@gmail.com>
2024-09-18 10:17:02 -0600

    Feat: init project.

---
+++ src/test/java/com/aceros_duran/pos/PosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aceros_duran.pos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
